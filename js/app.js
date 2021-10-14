2c7564ffe07bb0698a0af9117d2e3602fb40666c
alex-hyman37 <alex.hyman@comcast.net>
2021-10-14 13:58:47 -0400

    mouseover/mouseout event listener

--- js/app.js
+++ js/app.js
@@ -2,4 +2,23 @@ particlesJS.load('particles-js', 'js/particles.json', function(){
   console.log('particles.json loaded...');
 });
 
-smoothScroll.init(); //arrow//
+
+let frontTitle = document.querySelector(".front-title");
+let moreInfo = document.querySelector(".more-info");
+let card = document.querySelector(".card");
+
+  card.addEventListener('mouseover', (event) => {
+    setTimeout(() => {
+      frontTitle.style.display = 'none';
+      moreInfo.style.display = 'block';
+    }, 200);
+    
+  });
+  
+  card.addEventListener('mouseout', (event) => {
+    setTimeout(() => {
+      frontTitle.style.display = 'block';
+      moreInfo.style.display = 'none';
+    }, 200);
+    
+  });

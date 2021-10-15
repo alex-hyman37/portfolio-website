12d832e7d89fc57372d0d8fb75368db73dc4dae4
alex-hyman37 <alex.hyman@comcast.net>
2021-10-15 15:01:52 -0400

    updated event listeners

--- js/app.js
+++ js/app.js
@@ -3,36 +3,37 @@ particlesJS.load('particles-js', 'js/particles.json', function(){
 });
 
 
-let frontTitle = document.querySelector(".front-title");
-let moreInfo = document.querySelector(".more-info");
-let card = document.querySelector(".card");
+// Hover card
+let cards = document.querySelectorAll(".card");
 let projects = document.querySelector(".projects");
 
+cards.forEach(card => {
 
-  projects.addEventListener('mouseover', (event) => {
-    if(event.target.className === 'card') {
-      setTimeout(() => {
-        frontTitle.style.display = 'none';
-        moreInfo.style.display = 'block';
-      }, 200);
-
-      if(event.target.className !== 'card') {
-        setTimeout(() => {
-          frontTitle.style.display = 'block';
-          moreInfo.style.display = 'none';
-        }, 200);
-      }
-    } else {
-      return;
-    }
-  });
+  let frontTitle = card.querySelector(".front-title");
+  let moreInfo = card.querySelector(".more-info");
   
-  // card.addEventListener('mouseout', (event) => {
-  //   if(event.target.matches('.card'))
-  //   setTimeout(() => {
-  //     frontTitle.style.display = 'block';
-  //     moreInfo.style.display = 'none';
-  //   }, 200);
-    
-  // });
+  card.addEventListener('mouseover', (event) => {
+    setTimeout(() => {
+      frontTitle.style.display = 'none';
+      moreInfo.style.display = 'block';
+    }, 200);
+  });
+
+  card.addEventListener('mouseout', (event) => {
+    setTimeout(() => {
+      frontTitle.style.display = 'block';
+      moreInfo.style.display = 'none';
+    }, 200);
+  });
+
+  moreInfo.addEventListener('click', (e) => {
+    card.classList.toggle('is-flipped');
+  });
+
+});
+
+// Flip card
+
+
+
 

10dffd68daee21c639124b4c1caa344e429b8d68
alex-hyman37 <alex.hyman@comcast.net>
2021-10-16 14:07:10 -0400

    hover changes

--- js/app.js
+++ js/app.js
@@ -3,34 +3,24 @@ particlesJS.load('particles-js', 'js/particles.json', function(){
 });
 
 
-// Hover card
-let cards = document.querySelectorAll(".card");
-let projects = document.querySelector(".projects");
+// // Hover card
+// let cards = document.querySelectorAll(".card");
+// let projects = document.querySelector(".projects");
 
-cards.forEach(card => {
+// cards.forEach(card => {
 
-  let frontTitle = card.querySelector(".front-title");
-  let moreInfo = card.querySelector(".more-info");
+//   let frontTitle = card.querySelector(".front-title");
+//   let moreInfo = card.querySelector(".more-info");
   
-  card.addEventListener('mouseover', (event) => {
-    setTimeout(() => {
-      frontTitle.style.display = 'none';
-      moreInfo.style.display = 'block';
-    }, 200);
-  });
+//   card.addEventListener('mouseover', (event) => {
+//       card.classList.toggle('hover');
+//   });
 
-  card.addEventListener('mouseout', (event) => {
-    setTimeout(() => {
-      frontTitle.style.display = 'block';
-      moreInfo.style.display = 'none';
-    }, 200);
-  });
+//   card.addEventListener('mouseout', (event) => {
+//       card.classList.toggle('hover');
+//   });
 
-  moreInfo.addEventListener('click', (e) => {
-    card.classList.toggle('is-flipped');
-  });
-
-});
+// });
 
 // Flip card
 
